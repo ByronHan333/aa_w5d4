212312a5355c112b10067a6eaa1bd389992eff8f
ByronHan333 <zhan12@dons.usfca.edu>
2022-11-03 15:21:05 -0700

    add

--- w5d4_association/app/models/course.rb
+++ w5d4_association/app/models/course.rb
@@ -1,2 +1,39 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :Enrollment,
+  dependent: :destroy
+
+  has_many :enrolled_students, through: :enrollments, source: :student
+
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id,
+  class_name: :User
+
+  belongs_to :prerequisite,
+  primary_key: :id,
+  foreign_key: :prereq_id,
+  class_name: :Course,
+  optional: true
+
+  has_one :next_course,
+  primary_key: :id,
+  foreign_key: :prereq_id,
+  class_name: :Course
+
+  # has_one :
+
 end
